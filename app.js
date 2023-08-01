c431d9277fc0260382420a557e552518ec9c306c
Smitty510 <smithpatrick.510@gmail.com>
2023-08-01 17:26:24 -0400

    gg

--- app.js
+++ app.js
@@ -26,25 +26,29 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 //Exercise 3 Section============================================================
+console.log("EXERCISE 3:\n==========\n");
 
 ////Ignore Even 'Do-While'
+console.log("Ignore Even 'Do-While':\n==========\n");
 let y = 1
 do {
-    if (y % 2 != 0) {
+    if (y % 2 !== 0) {
         console.log(y);
     }
     y++;
-}   while(y <= 100);
+}   while(y < 100);
 
 ////Ignore Even 'While'=====================================
+console.log("Ignore Even 'While':\n==========\n");
 let z = 1;
-while(z <= 100) {
-    if(z % 2 != 0) {
+while(z < 100) {
+    if(z % 2 !== 0) {
         console.log(z);
-    }
-}   z++;
+    }z++;
+}   
 
 ////Fizz Buzz 'Do-While'====================================
+console.log("Fizz Buzz 'Do-While':\n==========\n");
 let x = 1;
 do {
   if (x % 3 === 0 && x % 5 === 0) {
@@ -61,6 +65,7 @@ do {
 } while (x <= 100);
 
 ////Fizz Buzz 'While'=======================================
+console.log("Fizz Buzz 'While':\n==========\n");
 let number = 1;
 
 while (number <= 100) {
@@ -78,7 +83,7 @@ while (number <= 100) {
 }
 
 //Exercise 4 Section============================================================
-
+console.log("EXERCISE 4:\n==========\n");
 ////Find Value
 let value = Math.round(Math.random() * 500);
 let b = Math.round(Math.random() * (500 - 100) + 100);
@@ -98,6 +103,7 @@ if (!found) {
 }
 
 //Exercise 5 Section============================================================
+console.log("EXERCISE 5:\n==========\n");
 
 ////Customized 'FizzBuzz'
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
@@ -107,11 +113,11 @@ let start = Math.round(Math.random() * (10 - 1) + 1);
 
 for (let i = start; i <= n; i++) {
   if (i % fizzDivisor == 0 && i % buzzDivisor == 0) {
-    console.log('${i}FizzBuzz');
+    console.log(`${i} FizzBuzz`);
   } else if (i % fizzDivisor == 0) {
-    console.log('${i}Fizz');
+    console.log(`${i} Fizz`);
   } else if (i % buzzDivisor == 0) {
-    console.log('${i}Buzz');
+    console.log(`${i} Buzz`);
   } else {
     console.log(i);
   }
